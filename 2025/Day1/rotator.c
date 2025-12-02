3cf5bf3e67495cc0cdb39908ad8436de55554a94
Ben Thompson <bthomps5@nd.edu>
2025-12-02 11:24:48 -0500

    working stage 2 of day 1

--- 2025/Day1/rotator.c
+++ 2025/Day1/rotator.c
@@ -3,6 +3,7 @@
 
 int zeroes = 0;
 long int pos = 50;
+long int newpos = 50;
 
 int main(int argc, char *argv[]) {
   FILE *stream;
@@ -20,35 +21,31 @@ int main(int argc, char *argv[]) {
     perror("fopen");
     exit(EXIT_FAILURE);
   }
-  printf("Starting at %d...\n",pos);
   while ((nread = getline(&line, &size, stream)) != -1) {
     char dir = line[0];
     long int num;
     num = strtol(&line[1], NULL, 10);
-    while(num > 100) {
-      num = num % 100; // here we eliminate the excess 100-position rotations that just put us back where we started
-    }
+//    while(num >= 100) {
+//      num = num % 100; // here we eliminate the excess 100-position rotations that just put us back where we started
+//      zeroes += 1; // every 100 positions crosses zero once
+//    }
     switch (dir) {
       case 'R':
         // turn right (count up)
-        pos += num;
-        pos %= 100; //cancel out 100 if we went over it
-        printf("Moved %ld to the %c to point at %ld\n",num,dir,pos);
-        // increment zero if we are at zeroes
-        if (pos == 0) {
-          zeroes += 1;
+        while(num > 0) {
+          pos += 1;
+          num -= 1;
+          if (pos == 100) {pos = 0;}
+          if (pos == 0) {zeroes += 1;}
         }
         break;
       case 'L':
         // turn left (count down)
-        pos -= num;
-        if (pos < 0) {
-          pos = 100 - labs(pos);
-        }
-         printf("Moved %ld to the %c to point at %ld\n",num,dir,pos);
-        // increment zeroes if we are at zero
-        if (pos == 0) {
-          zeroes += 1;
+        while (num > 0) {
+          pos -= 1;
+          num -= 1;
+          if (pos == -1) {pos = 99;}
+          if (pos == 0) {zeroes += 1;}
         }
         break;
       default:
